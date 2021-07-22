e93f7a8921d731e5a4b1b066633a7a0a83a7b697
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-07-22 17:55:17 +0530

    boilerplate files added

---
+++ particle-animation/script.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvasOne');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
